aaa66a42a31781b733b76839819387c4ac91bf4f
dtthor <d.t.thorwaldson@gmail.com>
2017-10-27 11:49:13 -0500

    Set up search bar component with state

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyAsq3c34GtQhYHBPG8xR_m-eIwgtUg0YVE';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      this.setState({ videos });
+      //this.setState({ videos: videos})
+    });
+  }
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      </div>
+    );
+  }
+}
+
+
+
+
+//Take this component's generated HTML
+// and put it on the page
+ReactDOM.render(<App />, document.querySelector('.container'));
